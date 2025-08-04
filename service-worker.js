f1eb0df5f42ea0eaa71a84a918c0cbf0c3201a01
funortehub <ian.santos2639@soufunorte.com.br>
2025-08-04 20:26:10 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,74 @@
+const CACHE_NAME = 'vita-integrare-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html', // Ou o nome do seu arquivo HTML principal
+  '/manifest.json',
+  '/icone.jpg',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Open+Sans:wght@300;400;600&display=swap',
+  'https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css',
+  'https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth-compat.js',
+  'https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore-compat.js',
+  'https://www.gstatic.com/firebasejs/9.6.1/firebase-storage-compat.js',
+  'https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/locales/pt-br.js',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and can only be consumed once. We must clone it so that
+            // the browser can consume the original response and we can
+            // consume the clone.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
