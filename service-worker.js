63a2fd5ddfc9b692aca58c08c0dbae4ca6b1cbab
funortehub <ian.santos2639@soufunorte.com.br>
2025-08-05 14:51:49 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'vita-integrare-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icone.jpg',
+  '/styles.css', // Adicione aqui seus arquivos CSS
+  '/script.js',  // Adicione aqui seus arquivos JavaScript
+  // Adicione outros arquivos que você deseja armazenar em cache
+];
+
+// Instala o Service Worker e faz o cache dos arquivos
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Ativa o Service Worker e limpa caches antigos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepta requisições e retorna do cache se disponível
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
