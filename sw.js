8b45e2fedc7251fbb6a7714f054619579beadb63
funortehub <ian.santos2639@soufunorte.com.br>
2025-08-04 22:29:51 -0300

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+  'https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/locales/pt-br.js',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
